a525e806a965886c39532bd0d25d555d9874d502
Brea Grindley <breagrindley@Breas-MacBook-Pro.local>
2023-05-14 20:11:55 +0930

    revised code, tidied

--- WeatherAPiTest/script.js
+++ WeatherAPiTest/script.js
@@ -7,11 +7,34 @@ async function fetchWeather(city) {
   return data;
 }
 
-async function main() {
-  const city = "Adelaide";
-  const weatherData = await fetchWeather(city);
-
-  console.log(weatherData);
+async function fetchWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
+
+async function main() {
+  const searchInput = document.getElementById("search-box");
+  const searchButton = document.getElementById("search-button");
+
+  searchButton.addEventListener("click", async () => {
+    const city = searchInput.value;
+    const weatherData = await fetchWeather(city);
+
+    // Display the city name
+    document.getElementById("city-name").innerHTML = `<h2>${weatherData.name}</h2>`;
+
+    // Display the wind speed
+    document.getElementById("wind-speed").innerHTML = `<p>Wind speed: ${weatherData.wind.speed} mph</p>`;
+
+    // Display the humidity
+    document.getElementById("humidity").innerHTML = `<p>Humidity: ${weatherData.main.humidity}%</p>`;
+
+    // Display the current temperature
+    const celsius = (weatherData.main.temp - 273.15);
+    document.getElementById("current-temperature").innerHTML = `<p>Current temperature: ${celsius}°C</p>`;
+  });
+};
 
 main();
